a62e5cd58374d62ee42b51e4a15cf50c161f2641
sarahflestari <sarahfauziah17@gmail.com>
2018-09-28 21:12:15 +0700

    Modul 1
    Layout Stuff

---
+++ HelloToast/app/src/main/java/com/sarahfauziahlestari/hellotoast/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sarahfauziahlestari.hellotoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+    }
+
+    public void countUp(View view) {
+    }
+}
